be3f16ce241f953e6ad1658737fc525be415cca7
Jonas Greifenhain <cadivus@daverkomp.de>
2025-06-13 11:38:46 -0700

    Add changes from server

--- hello_world/main.cu
+++ hello_world/main.cu
@@ -1,29 +1,36 @@
 #include <stdio.h>
 
-
-// Needed for LLVM Offload:
+#include <initializer_list>
 #ifdef __clang__
 #include <offload/cuda_runtime.h>
 #endif
 
 // CUDA kernel function: Each thread will execute this function
-__global__ void helloKernel() {
-    // Print from the GPU device. 
+__global__ void helloKernel(int *A) {
+    // Print from the GPU device.
     // Only one thread (thread index 0) will print to avoid duplicate messages.
     if (threadIdx.x == 0 && blockIdx.x == 0) {
-        printf("Hello, World from the GPU!\n");
+	      *A = 42;
+	      // Broken
+        // printf("Hello, World from the GPU %i!\n", 0);
     }
 }
 
 int main() {
+    int *A;
+    int R = 5, P = 7;
+    cudaMalloc(&A, 4);
+    cudaMemcpy(A, &P, 4, cudaMemcpyHostToDevice);
     // Launch the kernel with one block and one thread.
-    helloKernel<<<1, 1>>>();
+    helloKernel<<<1, 1>>>(A);
 
     // Wait for the GPU to finish before accessing on host
     cudaDeviceSynchronize();
 
+    cudaMemcpy(&R, A, 4, cudaMemcpyDeviceToHost);
+
     // Optionally, also print a message from the host
-    printf("Hello, World from the CPU!\n");
+    printf("Hello, World from the CPU: %i!\n", R);
 
     return 0;
 }
