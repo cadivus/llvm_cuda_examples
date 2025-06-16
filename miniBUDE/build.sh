7c19b63ec7fa586fc062bdb4132035164dde56d7
Jonas Greifenhain <cadivus@daverkomp.de>
2025-06-16 11:32:53 -0700

    Add build.ninja for running this on remote machines

--- miniBUDE/build.sh
+++ miniBUDE/build.sh
@@ -8,8 +8,13 @@ BUILD_DIR="./build"
 
 rm -rf "$BUILD_DIR"
 
+if [ "$model" = "cuda_native" ]; then
+    flags="-DCMAKE_CUDA_COMPILER=$CMAKE_CUDA_COMPILER -DCUDA_ARCH=$GPU_ARCH"
+    model="cuda"
+fi
+
 cmake -B "$BUILD_DIR" -H. \
     -DCMAKE_BUILD_TYPE=Release \
-    -DMODEL="$model" $flags &>>"$log"
+    -DMODEL="$model" $flags
 
 cmake --build "$BUILD_DIR" -j "$(nproc)"
